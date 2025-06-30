07e5a3df8ddcf79bf532c3aee9c53e7c444c6086
akash <akashkattempudi002@gmail.com>
2025-06-30 14:38:39 +0530

    new

--- demoBrowser.py
+++ demoBrowser.py
@@ -1,50 +1,29 @@
-import time
 from selenium import webdriver
-driver = webdriver.Chrome()
+
+#chrome driver
+from selenium.webdriver.chrome.service import Service
+#-- Chrome
+service_obj = Service("/Users/rahulshetty/documents/chromedriver")
+driver = webdriver.Chrome(service=service_obj)
+
+#-- Firefox
+# service_obj = Service("/Users/rahulshetty/documents/geckodriver")
+# driver = webdriver.Firefox(service=service_obj)
+
+#-- Edge
+# service_obj = Service("/Users/rahulshetty/documents/msedgedriver")
+# driver = webdriver.Edge(service=service_obj)
 
 
-driver.get("https://rahulshettyacademy.com")
+
+
 driver.maximize_window()
+driver.get("https://rahulshettyacademy.com")
 print(driver.title)
 print(driver.current_url)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-time.sleep(2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+driver.minimize_window()
+driver.back()
+driver.refresh()
+driver.forward()
+driver.close()
